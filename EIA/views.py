57cdb8b8a641bd76bd6df8d15ea0b6b5d344cf4c
Agerdiu <2858092937@qq.com>
2018-08-10 22:13:07 +0800

    update some fix

--- EIA/views.py
+++ EIA/views.py
@@ -4,11 +4,18 @@ import json
 from scipy.io import loadmat
 import scipy.io as sio
 import random
+handjointspath = "U:\\hand\\joints\\video_1\\"
+handboundspath = "U:\\hand\\bound\\video_1\\"
 def index(request):
     return render(request, 'EIA/login.html', context={})
 
 def people(request):
-    people = Peopledata.objects.get(id=random.randint(0, Peopledata.objects.count()))
+    while(1):
+        people = Peopledata.objects.get(id=random.randint(0, Peopledata.objects.count()-1))
+        if people.token == 0 :
+            break
+    people.token = 1
+    people.save()
     data = json.loads(people.datajson)
     count = 0
     imgurl = "/static/EIA/people-images/" + people.dataname +".jpg"
@@ -17,16 +24,20 @@ def people(request):
     return render(request, 'EIA/Marking.html', context={'json': people.datajson, 'num': count,'name': people.dataname,'imgurl':imgurl})
 
 def hand(request):
-    hand = Handsdata.objects.order_by('?')[:1]
+    while (1):
+        hand = Handsdata.objects.get(id=random.randint(0, Handsdata.objects.count() - 1))
+        if hand.token == 0:
+            break
+    hand.token = 1
+    hand.save()
     try:
-        m = loadmat("U:\\hand\\joints\\video_1\\" + hand[0].dataname + ".mat")
-        b = loadmat("U:\\hand\\bound\\video_1\\" + hand[0].dataname + ".mat")
+        m = loadmat(handjointspath + hand.dataname + ".mat")
+        b = loadmat("U:\\hand\\bound\\video_1\\" + hand.dataname + ".mat")
     except:
-        print("call except")
-        sio.savemat("U:\\hand\\joints\\video_1\\" + hand[0].dataname + ".mat", {'joints_2d': [[]]})
-        sio.savemat("U:\\hand\\bound\\video_1\\" + hand[0].dataname + ".mat", {'bound': [0, 0, 100, 100]})
-        m = loadmat("U:\\hand\\joints\\video_1\\" + hand[0].dataname + ".mat")
-        b = loadmat("U:\\hand\\bound\\video_1\\" + hand[0].dataname + ".mat")
+        sio.savemat(handjointspath + hand.dataname + ".mat", {'joints_2d': [[]]})
+        sio.savemat(handboundspath + hand.dataname + ".mat", {'bound': [0, 0, 100, 100]})
+        m = loadmat(handjointspath + hand.dataname + ".mat")
+        b = loadmat(handboundspath + hand.dataname + ".mat")
     newjson = {}
     array = []
     for item in m["joints_2d"]:
@@ -35,7 +46,6 @@ def hand(request):
         array.append(1)
     newjson["keypoint_annotations"] = {}
     newjson["keypoint_annotations"]["hand1"] = array
-    print(array)
     array2 = []
     for item in b["bound"]:
         array2.append(float(item[0]))
@@ -44,36 +54,31 @@ def hand(request):
         array2.append(float(item[3]))
     newjson["bound"] = array2
     return render(request, 'EIA/Markinghand.html',
-                  context={'json': json.dumps(newjson), 'num': 1, 'name': hand[0].dataname})
-
+                  context={'json': json.dumps(newjson), 'num': 1, 'name': hand.dataname})
 def updatepeople(request):
-    print("get")
     if request.POST:
-        print(request.POST)
         data = json.loads(request.POST.get("data"))
         down = Peopledata.objects.get(dataname=data[0]["dataname"])
         downjson = json.loads(down.datajson)
-        print(downjson)
         i = 0
         for item in downjson:
             print(item)
             item = data[i+1]["values"]
             i = i + 1
         down.datajson = json.dumps(downjson)
-        print(downjson)
+        down.token = 0
         down.save()
     return index(request)
 def updatehand(request):
     if request.POST:
-        print(request.POST)
         data = json.loads(request.POST.get("data"))
-        print(data)
         dataname = data[0]['dataname']
-        m = loadmat("U:\\hand\\joints\\video_1\\" + dataname + ".mat")
-        b = loadmat("U:\\hand\\bound\\video_1\\" + dataname + ".mat")
-        print(data[1]['values'])
+        hand = Handsdata.objects.get(dataname=dataname)
+        hand.token = 0
+        hand.save()
+        m = loadmat(handjointspath + dataname + ".mat")
+        b = loadmat(handboundspath + dataname + ".mat")
         i=0;j = 0
-        print(len(data[1]['values']))
         joints = []
         while(j<len(data[1]['values'])):
             array = []
@@ -82,9 +87,8 @@ def updatehand(request):
             array.append(data[1]['values'][j+2])
             joints.append(array)
             j=j+3
-        m['joints_2d'] = joints;
+        m['joints_2d'] = joints
         b['bound'] = data[2]['values']
-        sio.savemat("U:\\hand\\joints\\video_1\\" + dataname + ".mat", m)
-        sio.savemat("U:\\hand\\bound\\video_1\\" + dataname + ".mat", b)
-        print(m)
+        sio.savemat(handjointspath + dataname + ".mat", m)
+        sio.savemat(handboundspath + dataname + ".mat", b)
     return index(request)
